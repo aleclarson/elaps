7b6246342c0d64c115e83926ce793bd39243c0e7
aleclarson <alec.stanford.larson@gmail.com>
2018-07-17 19:43:07 -0400

    feat: add `pause` and `time` methods

--- index.js
+++ index.js
@@ -20,34 +20,47 @@ class Stopwatch {
     this.lap = null;
     this.laps = 0;
     this.total = 0;
+    this.paused = false;
     this.started = null;
     return this;
   }
   start() {
     if (this.started === null) {
+      this.paused ? (this.paused = false) : (this.lap = 0);
       this.started = process.hrtime();
     }
     return this;
   }
+  time() {
+    if (this.started === null) return 0;
+    const time = process.hrtime(this.started);
+    return time[0] * 1e3 + time[1] * 1e-6;
+  }
+  pause() {
+    if (this.started !== null) {
+      this.lap += this.time();
+      this.paused = true;
+      this.started = null;
+    }
+    return this;
+  }
   stop() {
-    if (this.started === null) {
+    if (this.started === null && !this.paused) {
       return this;
     }
 
-    let time = process.hrtime(this.started);
-    this.lap = time =
-      time[0] * 1e3 + time[1] * 1e-6;
-
+    let time = this.lap += this.time();
     this.laps += 1;
     this.total += time;
+    this.paused = false;
     this.started = null;
  
     if (this.msg) {
       time = Number(time.toFixed(time < 100 ? 1 : 0));
       if (elapsedRE.test(this.msg)) {
-        console.log(format(this.msg.replace(elapsedRE, '%Oms'), time));
+        console.log(format(this.msg.replace(elapsedRE, '%O ms'), time));
       } else {
-        console.log(format(this.msg + ' (%Oms)', time));
+        console.log(format(this.msg + ' (%O ms)', time));
       }
     }
     return this;
